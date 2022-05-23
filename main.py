02e12525397a584ee8696b4fdc26acbca566879e
Marek Brandt <marek7119@gmail.com>
2022-05-23 14:08:59 +0200

    actual best model

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from agent import Agent
 if __name__ == "__main__":
     env = gym.make("CarRacing-v1")
     agent = Agent(env)
-    agent.train()
-    # agent.play_with_model('nasz3.pth')
+    # agent.train()
+    agent.play_with_model('nasz3.pth')
     env.close()
 
 
